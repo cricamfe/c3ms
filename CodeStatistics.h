1e1d1ab65b0c394d1a82448f12478bc23c081926
cricamfe <cricamfe@ac.uma.es>
2023-11-28 08:55:59 +0100

    [UPDATE] Refactor CodeStatistics class to use string_view and improve performance

--- CodeStatistics.h
+++ CodeStatistics.h
@@ -1,102 +1,81 @@
-/* Copyright (c) 2009, 2010 Basilio B. Fraguela. Universidade da CoruÃ±a */
-
 #ifndef CODESTATISTICS_H
 #define CODESTATISTICS_H
 
 #include <set>
 #include <string>
+#include <string_view>
+#include <algorithm>
 
 class CodeStatistics {
-  
-  struct ltstr2 { 
-    bool operator()(const std::string& s1, const std::string& s2) const
-    {
-      return s1.compare(s2) < 0;
-    }
-  };
-
-  typedef std::set<std::string, ltstr2> CSSet;
-  
 public:
-  
-  typedef unsigned int StatSize;
+    using StatSize = unsigned int;
+    using CSSet = std::set<std::string, std::less<>>; // Uso de std::less<> para simplificar
 
-  CodeStatistics() {
-    reset();
-  }
-  
-  void reset() {
-    types = 0;
-    constants = 0;
-    identifiers = 0;
-    cspecs = 0;
-    keywords = 0;
-    operators = 0;
-    conditions = 0;
-    
-    typesSet.clear();
-    constantsSet.clear();
-    identifiersSet.clear();
-    cspecsSet.clear();
-    keywordsSet.clear();
-    operatorsSet.clear();
-  }
+    CodeStatistics() = default; // Constructor por defecto
 
-  void type(const char *p)       {       types++;       typesSet.insert(std::string(p)); }
-  void constant(const char *p)   {   constants++;   constantsSet.insert(std::string(p)); }
-  void identifier(const char *p) { identifiers++; identifiersSet.insert(std::string(p)); }
-  void cspec(const char *p)      {      cspecs++;      cspecsSet.insert(std::string(p)); }
-  void keyword(const char *p)    {    keywords++;    keywordsSet.insert(std::string(p)); }
-  void op(const char *p)         {   operators++;   operatorsSet.insert(std::string(p)); }
+    void reset() {
+        types = constants = identifiers = cspecs = keywords = operators = conditions = 0;
+        typesSet.clear();
+        constantsSet.clear();
+        identifiersSet.clear();
+        cspecsSet.clear();
+        keywordsSet.clear();
+        operatorsSet.clear();
+    }
 
-  void decOp()      { operators--; }
-  void cond()       { conditions++; }
+    void type(std::string_view p)       { types++;       typesSet.insert(std::string(p)); }
+    void constant(std::string_view p)   { constants++;   constantsSet.insert(std::string(p)); }
+    void identifier(std::string_view p) { identifiers++; identifiersSet.insert(std::string(p)); }
+    void cspec(std::string_view p)      { cspecs++;      cspecsSet.insert(std::string(p)); }
+    void keyword(std::string_view p)    { keywords++;    keywordsSet.insert(std::string(p)); }
+    void op(std::string_view p)         { operators++;   operatorsSet.insert(std::string(p)); }
 
-  StatSize getTypes()       const { return types; }
-  StatSize getConstants()   const { return constants; }
-  StatSize getIdentifiers() const { return identifiers; }
-  StatSize getCspecs()      const { return cspecs; }
-  StatSize getKeywords()    const { return keywords; }
-  StatSize getOps()         const { return operators; }
-  StatSize getConds()       const { return conditions; }
+    void decOp()                        { operators--; }
+    void cond()                         { conditions++; }
 
-  StatSize getOperands()    const { return types + constants + identifiers; }
-  StatSize getOperators()   const { return cspecs + keywords + operators;   }
+    StatSize getTypes()       const { return types; }
+    StatSize getConstants()   const { return constants; }
+    StatSize getIdentifiers() const { return identifiers; }
+    StatSize getCspecs()      const { return cspecs; }
+    StatSize getKeywords()    const { return keywords; }
+    StatSize getOps()         const { return operators; }
+    StatSize getConds()       const { return conditions; }
 
-  StatSize getUniqueTypes()       const { return typesSet.size(); }
-  StatSize getUniqueConstants()   const { return constantsSet.size(); }
-  StatSize getUniqueIdentifiers() const { return identifiersSet.size(); }
-  StatSize getUniqueCspecs()      const { return cspecsSet.size(); }
-  StatSize getUniqueKeywords()    const { return keywordsSet.size(); }
-  StatSize getUniqueOps()         const { return operatorsSet.size(); }
-  
-  StatSize getUniqueOperands() const  { return getUniqueTypes()  + getUniqueConstants() + getUniqueIdentifiers(); }
-  StatSize getUniqueOperators() const { return getUniqueCspecs() + getUniqueKeywords()  + getUniqueOps();   }
+    StatSize getOperands()    const { return types + constants + identifiers; }
+    StatSize getOperators()   const { return cspecs + keywords + operators; }
 
-  CodeStatistics& operator+= (const CodeStatistics& other) {
-    types       += other.types;
-    constants   += other.constants;
-    identifiers += other.identifiers;
-    cspecs      += other.cspecs;
-    keywords    += other.keywords;
-    operators   += other.operators;
-    conditions  += conditions;
+    StatSize getUniqueTypes()       const { return typesSet.size(); }
+    StatSize getUniqueConstants()   const { return constantsSet.size(); }
+    StatSize getUniqueIdentifiers() const { return identifiersSet.size(); }
+    StatSize getUniqueCspecs()      const { return cspecsSet.size(); }
+    StatSize getUniqueKeywords()    const { return keywordsSet.size(); }
+    StatSize getUniqueOps()         const { return operatorsSet.size(); }
 
-    copy(other.typesSet.begin(),       other.typesSet.end(),       inserter(typesSet,       typesSet.begin()));
-    copy(other.constantsSet.begin(),   other.constantsSet.end(),   inserter(constantsSet,   constantsSet.begin()));
-    copy(other.identifiersSet.begin(), other.identifiersSet.end(), inserter(identifiersSet, identifiersSet.begin()));
-    copy(other.cspecsSet.begin(),      other.cspecsSet.end(),      inserter(cspecsSet,      cspecsSet.begin()));
-    copy(other.keywordsSet.begin(),    other.keywordsSet.end(),    inserter(keywordsSet,    keywordsSet.begin()));
-    copy(other.operatorsSet.begin(),   other.operatorsSet.end(),   inserter(operatorsSet,   operatorsSet.begin()));
+    StatSize getUniqueOperands() const  { return getUniqueTypes() + getUniqueConstants() + getUniqueIdentifiers(); }
+    StatSize getUniqueOperators() const { return getUniqueCspecs() + getUniqueKeywords() + getUniqueOps(); }
 
-    return *this;
-  }
-  
+    CodeStatistics& operator+= (const CodeStatistics& other) {
+        types += other.types;
+        constants += other.constants;
+        identifiers += other.identifiers;
+        cspecs += other.cspecs;
+        keywords += other.keywords;
+        operators += other.operators;
+        conditions += other.conditions;
+
+        typesSet.insert(other.typesSet.begin(), other.typesSet.end());
+        constantsSet.insert(other.constantsSet.begin(), other.constantsSet.end());
+        identifiersSet.insert(other.identifiersSet.begin(), other.identifiersSet.end());
+        cspecsSet.insert(other.cspecsSet.begin(), other.cspecsSet.end());
+        keywordsSet.insert(other.keywordsSet.begin(), other.keywordsSet.end());
+        operatorsSet.insert(other.operatorsSet.begin(), other.operatorsSet.end());
+
+        return *this;
+    }
+
 private:
-  
-  StatSize types, constants, identifiers, cspecs, keywords, operators, conditions;
-
-  CSSet typesSet, constantsSet, identifiersSet, cspecsSet, keywordsSet, operatorsSet;
+    StatSize types = 0, constants = 0, identifiers = 0, cspecs = 0, keywords = 0, operators = 0, conditions = 0;
+    CSSet typesSet, constantsSet, identifiersSet, cspecsSet, keywordsSet, operatorsSet;
 };
 
 #endif //CODESTATISTICS_H
