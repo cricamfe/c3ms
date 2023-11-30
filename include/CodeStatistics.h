65e2c91d1d53b5bdc0e548fdcac943823db17929
cricamfe <cricamfe@ac.uma.es>
2023-11-30 13:11:03 +0100

    [UPDATE] CHANGELOG: - Swapped 'set' for 'unordered_map' to keep track of operators and operands as well. - Added the possibility to print the operators and operands as well as the accumulated occurrences of each one individually.

--- include/CodeStatistics.h
+++ include/CodeStatistics.h
@@ -1,15 +1,17 @@
 #ifndef CODESTATISTICS_H
 #define CODESTATISTICS_H
 
-#include <set>
+#include <unordered_map>
 #include <string>
 #include <string_view>
 #include <algorithm>
+#include <sstream>
+#include <iomanip>
 
 class CodeStatistics {
 public:
     using StatSize = unsigned int;
-    using CSSet = std::set<std::string, std::less<>>; // Uso de std::less<> para simplificar
+    using CSSet = std::unordered_map<std::string, int>; // Uso de std::unordered_map para contar ocurrencias
 
     CodeStatistics() = default; // Constructor por defecto
 
@@ -23,12 +25,12 @@ public:
         operatorsSet.clear();
     }
 
-    void type(std::string_view p)       { types++;       typesSet.insert(std::string(p)); }
-    void constant(std::string_view p)   { constants++;   constantsSet.insert(std::string(p)); }
-    void identifier(std::string_view p) { identifiers++; identifiersSet.insert(std::string(p)); }
-    void cspec(std::string_view p)      { cspecs++;      cspecsSet.insert(std::string(p)); }
-    void keyword(std::string_view p)    { keywords++;    keywordsSet.insert(std::string(p)); }
-    void op(std::string_view p)         { operators++;   operatorsSet.insert(std::string(p)); }
+    void type(std::string_view p)       { types++;       typesSet[std::string(p)]++; }
+    void constant(std::string_view p)   { constants++;   constantsSet[std::string(p)]++; }
+    void identifier(std::string_view p) { identifiers++; identifiersSet[std::string(p)]++; }
+    void cspec(std::string_view p)      { cspecs++;      cspecsSet[std::string(p)]++; }
+    void keyword(std::string_view p)    { keywords++;    keywordsSet[std::string(p)]++; }
+    void op(std::string_view p)         { operators++;   operatorsSet[std::string(p)]++; }
 
     void decOp()                        { operators--; }
     void cond()                         { conditions++; }
@@ -53,6 +55,55 @@ public:
 
     StatSize getUniqueOperands() const  { return getUniqueConstants() + getUniqueIdentifiers(); }
     StatSize getUniqueOperators() const { return getUniqueCspecs() + getUniqueKeywords() + getUniqueOps() + getUniqueTypes(); }
+
+    // Función para imprimir un conjunto de métricas
+    void printMetrics(std::ostringstream& result, const CSSet& set, const int nameWidth, const int valueWidth) const {
+        for (const auto& element : set) {
+            result << std::left << std::setw(nameWidth) << element.first << " " 
+                   << std::right << std::setw(valueWidth) << element.second << '\n';
+        }
+    }
+
+    void printHeader(std::ostringstream& result, const std::string& left_header, const std::string& right_header, const int& nameWidth, const int& valueWidth, const int& totalWidth) const {
+        result << std::string(totalWidth, '-') << "\n";
+        result << std::left << std::setw(nameWidth) << left_header;
+        result << std::right << std::setw(valueWidth + (nameWidth - valueWidth) / 2) << right_header << "\n";
+        result << std::string(totalWidth, '-') << "\n";
+    }
+
+    std::string printOperators() const {
+        const int nameWidth = 40; // Column width for metric names
+        const int valueWidth = 15; // Adjusted column width for metric values
+        const int totalWidth = 75; // Total width for both columns
+        std::ostringstream result;
+
+        // Adding header
+        printHeader(result, "Operators", "Occurrences", nameWidth, valueWidth, totalWidth);
+
+        // Combining all operator sets into a single table
+        printMetrics(result, cspecsSet, nameWidth, valueWidth);
+        printMetrics(result, keywordsSet, nameWidth, valueWidth);
+        printMetrics(result, operatorsSet, nameWidth, valueWidth);
+        printMetrics(result, typesSet, nameWidth, valueWidth);
+
+        return result.str();
+    }
+
+    std::string printOperands() const {
+        const int nameWidth = 40; // Column width for metric names
+        const int valueWidth = 15; // Adjusted column width for metric values
+        const int totalWidth = 75; // Total width for both columns
+        std::ostringstream result;
+
+        // Adding header
+        printHeader(result, "Operands", "Occurrences", nameWidth, valueWidth, totalWidth);
+        
+        // Combining all operand sets into a single table
+        printMetrics(result, constantsSet, nameWidth, valueWidth);
+        printMetrics(result, identifiersSet, nameWidth, valueWidth);
+
+        return result.str();
+    }
 
     CodeStatistics& operator+= (const CodeStatistics& other) {
         types += other.types;
